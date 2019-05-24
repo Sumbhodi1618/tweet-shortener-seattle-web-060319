14b0129b651309feaea63f21546fe4b9afddb841
Justin <sumbhodi1618@gmail.com>
2019-05-24 08:21:08 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+def dictionary
+  dictionary = {"hello"=>'hi',
+"to, two, too"=>'2',
+"for, four"=>'4',
+"be"=>'b',
+"you"=>'u',
+"at"=>'@',
+"and"=>'&' }
+end
+
+def word_substituter(tweet_one)
+  tweet_one_short
+end
+
+def bulk_tweet_shortener(tweets)
+  puts tweet_one_short
+   puts tweet_two_short
+    puts tweet_three_short
+     puts tweet_four_short
+      puts tweet_five_short
+end
+
+def selective_tweet_shortener(tweets)
+    if tweet_one >= tweet_one.length
+      tweet_one_short
+      tweet_two >= tweet_two.length
+      tweet_two short
+      tweet_three >= tweet_three.length
+      tweet_three_short
+      tweet_four >= tweet_four.length
+      tweeet_four_short
+      tweet_five >= tweet_five.length
+      tweet_five_short
+end
+end
