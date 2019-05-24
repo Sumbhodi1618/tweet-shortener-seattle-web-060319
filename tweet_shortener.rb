7eb01dd89a5aa44de1dde889ddf872e6de57dc7e
Justin <sumbhodi1618@gmail.com>
2019-05-24 21:39:44 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,12 @@
+require 'pry'
 def dictionary
-  dictionary = {"hello"=>'hi',
-"to, two, too"=>'2',
-"for, four"=>'4',
+  dictionary = {
+"hello"=>'hi',
+"to"=>'2',
+"two"=>'2',
+"too"=>'2',
+"for"=>'4',
+"four"=>'4',
 "be"=>'b',
 "you"=>'u',
 "at"=>'@',
@@ -9,27 +14,21 @@ def dictionary
 end
 
 def word_substituter(tweet_one)
-  tweet_one_short
+  array = []
+  tweet_one.split.map do |word|
+    if dictionary[word]
+      array << dictionary[word]
+    else
+      array << word
+    end
+  end
+  array.join(" ")
 end
 
 def bulk_tweet_shortener(tweets)
-  puts tweet_one_short
-   puts tweet_two_short
-    puts tweet_three_short
-     puts tweet_four_short
-      puts tweet_five_short
+      binding.pry
+  tweets.split.map
 end
-
 def selective_tweet_shortener(tweets)
-    if tweet_one >= tweet_one.length
-      tweet_one_short
-      tweet_two >= tweet_two.length
-      tweet_two short
-      tweet_three >= tweet_three.length
-      tweet_three_short
-      tweet_four >= tweet_four.length
-      tweeet_four_short
-      tweet_five >= tweet_five.length
-      tweet_five_short
-end
+
 end
