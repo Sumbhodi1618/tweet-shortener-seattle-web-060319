2f1847bf76968500cde32c63fc1214ae32ffb7e6
Justin <sumbhodi1618@gmail.com>
2019-05-25 17:21:57 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -6,6 +6,7 @@ def dictionary
 "two"=>'2',
 "too"=>'2',
 "for"=>'4',
+"For"=>'4',
 "four"=>'4',
 "be"=>'b',
 "you"=>'u',
@@ -26,9 +27,24 @@ def word_substituter(tweet_one)
 end
 
 def bulk_tweet_shortener(tweets)
-      binding.pry
-  tweets.split.map
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end
-def selective_tweet_shortener(tweets)
 
+def selective_tweet_shortener(tweet)
+    if tweet.length >= 140
+    puts word_substituter(tweet)
+    else
+      tweet
+    end
+  end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+    word_substituter(tweet)[0..136] + ("...")
+  else
+    word_substituter(tweet)
+  end
+end
 end
